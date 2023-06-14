ff3fa81cd2c24425932253960ee3a6dd32f52d16
Keith B. Smith <32456218+afewvowels@users.noreply.github.com>
2023-06-14 12:18:04 -0400

    close enough for now!

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,26 +20,43 @@ const Home = () => {
       let bee = document.createElement('div')
       bee.classList.add('bee')
 
-      let endPointX = Math.random() * 500 - 250;
-      console.log("calculated x: " + endPointX)
-      let endPointY = Math.random() * 500 - 250; // Random Y within a range
-      console.log("calculated y: " + endPointY)
+      let endPointX, endPointY = 0
+
+      do {
+        endPointX = Math.random() * 1500 - 750;
+      } while (endPointX < 250 && endPointX > -250)
+
+      do {
+        endPointY = Math.random() * 1500 - 750;
+      } while (endPointY < 250 && endPointY > -250)
+
+      let originX = Math.random() * 100 - 50;
+      let originY = Math.random() * 100 - 50;
+      originX = originX - 25
+      originY = originY - 25
+
       let rotation = Math.atan2(-endPointY, endPointX) * (180 / Math.PI);
+
+      let duration = Math.abs(Math.random() * 1000) + 500
 
       document.getElementById('beehiveContainer').appendChild(bee)
       bees.current.push(bee)
 
       anime({
         targets: bee,
-        translateX: [0, endPointX],
-        translateY: [0, endPointY],
+        translateX: [originX, endPointX],
+        translateY: [originY, endPointY],
         rotate: rotation + 'deg',
-        easing: 'linear',
-        duration: Math.random() * 3000 - 500,
+        easing: 'easeInQuad',
+        duration: duration,
+        scale: {
+          value: [0, Math.abs(Math.random()) + 0.5],
+          duration: 200
+        },
         opacity: {
           value: 0,
-          delay: 1000,
-          duration: 1000
+          delay: 200,
+          duration: duration
         },
         complete: () => {
           bee.remove()
